099a89a48c197dbc176bf253035c6b98f3f473ce
PushingTrooper <junis.milori@gmail.com>
2021-08-20 19:59:14 +0200

    first commit

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import './dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/categories-meals';
+
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+
+    final title = args['title']!;
+    final categoryId = args['id']!;
+    final meals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Text(meals[index].title);
+        },
+        itemCount: meals.length,
+      ),
+    );
+  }
+}
